b4490ba26c4a258c66d0ca7effb9c3baaeb4d94d
Niklas Koch <Niklaskoch1406@gmail.com>
2025-09-08 15:10:16 +0200

    added angular and spring update angular now

---
+++ DoenerBestellungBE/src/main/resources/db/migration/V1_1_0__create_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+
+                                          id int NOT NULL PRIMARY KEY,
+                                          name varchar(50),
+    email varchar(50),
+    date_of_birth timestamp
+
+    )
